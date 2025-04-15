dd64252aa0e610c05a7211bb59f126405374e29e
ibrahim-ashraf597 <ia9319138@gmail.com>
2025-04-15 09:16:51 +0200

    Update Fast Power.cpp

--- Fast Power.cpp
+++ Fast Power.cpp
@@ -1,14 +1,13 @@
 
-
-ll power(ll n, ll p,ll mod)
-{
-    if (!p)
+ll power(ll n, ll p, ll mod) {
+    if (p == 0)
         return 1;
-    ll a = power(n, p / 2) % mod;
-    a = a;
-    a %= mod;
-    if (p & 1)
-        return (a(n % mod)) % mod;
+
+    ll a = power(n, p / 2, mod);
+    a = (a * a) % mod;
+
+    if (p % 2 == 1)
+        return (a * (n % mod)) % mod;
     else
         return a;
 }
