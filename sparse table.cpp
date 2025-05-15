827af003f08afb1232d58455c6990b66db46f830
ibrahim-ashraf597 <ia9319138@gmail.com>
2025-05-16 00:37:52 +0300

    Update sparse table.cpp

--- sparse table.cpp
+++ sparse table.cpp
@@ -1,28 +1,26 @@
-int n, T[N][32] = {}, Log[N]; vector<int>a(N);
+const int N = 1e6 + 5, n2 = 1e3 + 55;
+
+int n, T[N][32] = {}, Log[N]; vector<int>a(N,1e18);
 int merge(int a, int  b) {
     return min(a, b);
 }
 void build() {
     for (int i = 0; i < n; i++)
         T[i][0] = a[i];
-    for (int pw = 1; (1 << pw) <= n; pw++) {
-        for (int i = 0; i + (1 << pw) <= n; i++) {
-            T[i][pw] = merge(T[i][pw - 1], T[i + (1 << (pw - 1))][pw - 1]);
+    for (int pw = 1; (1ll << pw) <= n; pw++) {
+        for (int i = 0; i + (1ll << pw) <= n; i++) {
+            T[i][pw] = merge(T[i][pw - 1], T[i + (1ll << (pw - 1))][pw - 1]);
         }
     }
 
     return;
 }
 int query(int l, int r) {
-    int sz = (r - l + 1); int ret = 0;
-    for (int i = 32; i >= 0; i--)
-    {
-        if (((sz >> i) & 1)) {
-            ret = merge(ret, T[l][i]); l += (1 << i);
-        }
-    }
-    return ret;
+    int len = r - l + 1;
+    int k = Log[len];
+    return merge(T[l][k], T[r - (1ll << k) + 1][k]);
 }
+
 void preCount() {
     Log[1] = 0;
     for (int i = 2; i < N; i++)
