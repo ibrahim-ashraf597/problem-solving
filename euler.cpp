0ba41f313ebee906dd7037121dfe0e6d409abed0
ibrahim-ashraf597 <ia9319138@gmail.com>
2025-06-16 03:30:18 +0300

    Add files via upload

---
+++ euler.cpp
@@ -0,0 +1,11 @@
+﻿int timer = 0;
+vector<int>in(N), out(N); vector<int>ad[N];
+void dfs(int u, int p) {
+    in[u] = ++timer;
+    for (auto x : ad[u])
+        if (p != x)
+            dfs(x, u);
+
+    out[u] = ++timer;
+
+};
