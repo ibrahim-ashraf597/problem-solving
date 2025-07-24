cad3240ae1e772e931372b7689a6d3fc54c76c3d
ibrahim-ashraf597 <ia9319138@gmail.com>
2025-07-24 13:13:48 +0300

    Update HLD.cpp

--- HLD.cpp
+++ HLD.cpp
@@ -1,7 +1,8 @@
-﻿
 struct HLD {
-    vector<int>sz, big, par, head, id, dep;
+    int sz[N], big[N], par[N], head[N], id[N], dep[N];
+
     void dfs(int v, const vector<vector<int>>& ad) {
+        sz[v] = 1;
         for (auto x : ad[v]) {
             if (x == par[v]) continue;
             par[x] = v;
@@ -10,39 +11,46 @@ struct HLD {
             sz[v] += sz[x];
             if (big[v] == 0 || sz[x] > sz[big[v]])
                 big[v] = x;
-
         }
     }
-    void fllatan(int v, const vector<vector<int>>& ad, int& next) {
+
+    void flatten(int v, const vector<vector<int>>& ad, int& next) {
         id[v] = next++;
-        if (big[v])
-            head[big[v]] = head[v], fllatan(big[v], ad, next);
+        if (big[v]) {
+            head[big[v]] = head[v];
+            flatten(big[v], ad, next);
+        }
         for (int x : ad[v]) {
-            if (x == par[v] || x == big[v])continue;
+            if (x == par[v] || x == big[v]) continue;
             head[x] = x;
-            fllatan(x, ad, next);
+            flatten(x, ad, next);
         }
     }
-    vector<pair<int, int>>path(int u, int v) {
-        vector<pair<int, int>>res;
-        while (1) {
-            if (head[u] == head[v]) {
-                if (dep[u] > dep[v])
-                    swap(u, v);
-                res.push_back({ id[u],id[v] });
-                return res;
-            }
-            if (dep[head[u]] > dep[head[v]])
-                swap(u, v);
-            res.push_back({ id[head[v]], id[v] });
-            v = par[head[v]];
-        }
 
+    vector<pair<int, int>> path(int u, int v) {
+        vector<pair<int, int>> res;
+        while (head[u] != head[v]) {
+            if (dep[head[u]] < dep[head[v]]) swap(u, v);
+            res.emplace_back(id[head[u]], id[u]);
+            u = par[head[u]];
+        }
+        if (dep[u] > dep[v]) swap(u, v);
+        res.emplace_back(id[u], id[v]);
+        return res;
     }
-    HLD(int n, const vector<vector<int>>& ad) :big(n + 1), par(n + 1), sz(n + 1, 1), head(n + 1, 1), id(n + 1), dep(n + 1) {
+
+    HLD(int n, const vector<vector<int>>& ad) {
+        for (int i = 0; i <= n; i++) {
+            sz[i] = 0;
+            big[i] = 0;
+            par[i] = 0;
+            head[i] = 0;
+            id[i] = 0;
+            dep[i] = 0;
+        }
         dfs(1, ad);
         int next = 0;
         head[1] = 1;
-        fllatan(1, ad, next);
+        flatten(1, ad, next);
     }
 };
