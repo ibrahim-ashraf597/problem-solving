93b871c015aada189cc89f2e9182f70f44b6a335
ibrahim-ashraf597 <ia9319138@gmail.com>
2025-08-30 00:22:36 +0300

    Add files via upload

---
+++ Trie.cpp
@@ -0,0 +1,35 @@
+﻿struct Trie {
+    struct node {
+        int pre; int end;
+        node* ch[26];
+        node() {
+            pre = 0; end = 0;
+            memset(ch, 0, sizeof ch);
+        }
+    };
+    node* root = new node();
+    void insert(string s) {
+        node* cur = root;
+        for (auto it : s) {
+            int idx = it - 'a';
+            if (cur->ch[idx] == 0) {
+                cur->ch[idx] = new node();
+            }
+            cur = cur->ch[idx];
+            cur->pre++;
+        }
+        cur->end++;
+    }
+    int count(string s) {
+        node* cur = root;
+        for (auto it : s) {
+            int idx = it - 'a';
+            if (cur->ch[idx] == 0) {
+                return 0;
+            }
+            cur = cur->ch[idx];
+
+        }
+        return cur->pre;
+    }
+};
