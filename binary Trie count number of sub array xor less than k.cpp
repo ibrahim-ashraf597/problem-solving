eafd81d5be209668d40d20eb5af1d4edd6173482
ibrahim-ashraf597 <ia9319138@gmail.com>
2025-09-10 00:27:41 +0300

    Update binary Trie count number of sub array xor less than k.cpp

--- binary Trie count number of sub array xor less than k.cpp
+++ binary Trie count number of sub array xor less than k.cpp
@@ -4,14 +4,14 @@
 #define el '\n'
 #define int ll
 using namespace std;
-const int N = 1e6 + 5, n22 = 501, mod = 1e9 + 7, M = 1e9 + 7, base = 31;
 int k;
 struct BinaryTrie {
     struct node {
         node* ch[2];
         int cnt = 0;
+        int fr[2];
         node() {
-
+            fr[0] = fr[1] = 0;
             ch[0] = ch[1] = 0;
 
         }
@@ -19,18 +19,32 @@ struct BinaryTrie {
     node* root = new node();
     void insert(int n) {
         node* cur = root;
-        cur->cnt++;                   
-        for (int i = 31; i >= 0; i--) {
+        cur->cnt++;
+        for (int i = 63; i >= 0; i--) {
             bool indx = (n >> i) & 1ll;
             if (cur->ch[indx] == 0) cur->ch[indx] = new node();
+            cur->fr[indx]++;
             cur = cur->ch[indx];
-            cur->cnt++;           
+            cur->cnt++;
         }
     }
+    void del(int n, int i, node* cur) {
+        if (i == -1)
+        {
+            cur->cnt--;
+            return;
+        }
+        bool idx = (n >> i) & 1;
+        del(n, i - 1, cur->ch[idx]);
+        cur->fr[idx]--;
+        cur->cnt--;
+        if (cur->fr[idx] == 0)
+            delete cur->ch[idx], cur->ch[idx] = 0;
 
+    }
     int query(int n) {
         node* cur = root; int ans = 0;
-        for (int i = 31; i >= 0; i--) {
+        for (int i = 63; i >= 0; i--) {
             bool idx = ((n >> i) & 1), idxk = ((k >> i) & 1);
             if (idxk) {
                 if (cur->ch[idx] != 0)
@@ -49,37 +63,29 @@ struct BinaryTrie {
         return ans;
     }
 
-    int MxXor(int n) {
-        node* cur = root; int val = 0;
-        for (int i = 31; i >= 0; i--) {
-            bool idx = (n >> i) & 1;
-            if (cur->ch[1 ^ idx] != 0) {
-                val += (1ll << i);
-                cur = cur->ch[1 ^ idx];
-            }
-            else {
-                cur = cur->ch[idx];
-            }
-        }
-        // if(cur->)
-        return val;
-
-    }
 };
-void solve() {
-    int n; cin >> n >> k;
-    BinaryTrie tr;
-    vector<int>a(n), pr(n);
-    tr.insert(0);
 
-    int c = 0;
-    for (int i = 0; i<n && cin >> a[i]; pr[i] = a[i], i ? pr[i] ^= pr[i - 1] : 0, i++);
-    for (int i = 0; i < n; i++) {
-      //  cout << tr.query(pr[i]) << el;
-        c += tr.query(pr[i]);
-        tr.insert(pr[i]);
-    }
-    cout << c << el;
+void solve() {
+    int n; BinaryTrie tr;
+    cin >> n;
+    while (n--) {
+        int op; cin >> op;
+        if (op == 1) {
+            int x; cin >> x;
+            tr.insert(x);
+        }
+        else if (op == 2) {
+            int x; cin >> x;
+            tr.del(x, 63, tr.root);
+        }
+        else {
+            int x, li;
+            cin >> x >> li; k = li;
+            cout << tr.query(x) << el;;
+        }
+
+   }
+
 
 }
 signed main() {
@@ -87,6 +93,6 @@ signed main() {
     cin.tie(0), cout.tie(0);
 
     int T = 1;
-   cin >> T;
+   //  cin >> T;
     while (T--) solve();
 }
