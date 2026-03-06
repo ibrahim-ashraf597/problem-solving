94b6862c8ad6a80f3275bb13fe5b2c210e015781
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-03-06 15:45:11 +0200

    Implement Persistent Segment Tree in PST.cpp

---
+++ PST.cpp
@@ -0,0 +1,93 @@
+struct Node {
+    ll val{};
+    Node *left, *right;
+ 
+    Node() {}
+ 
+    Node(int val) : val(val) {}
+ 
+    Node(Node *l, Node *r) {
+        left = l;
+        right = r;
+        if (left)val += left->val;
+        if (right)val += right->val;
+    }
+};
+ 
+struct PST {
+    int n;
+    vector<Node *> roots;
+ 
+    Node *build(int lx, int rx, vector<int> &a) {
+        if (lx == rx)return new Node(a[lx]);
+        int m = (lx + rx) >> 1;
+        auto left = build(lx, m, a);
+        auto right = build(m + 1, rx, a);
+        return new Node(left, right);
+    }
+ 
+    Node *update(Node *node, int lx, int rx, int i, int x) {
+        if (lx == rx) return new Node(x);
+        int m = (lx + rx) >> 1;
+        if (i <= m)
+            return new Node(update(node->left, lx, m, i, x), node->right);
+        else
+            return new Node(node->left, update(node->right, m + 1, rx, i, x));
+    }
+ 
+    ll query(Node *node, int lx, int rx, int l, int r) {
+        if (lx > r || rx < l) return 0;
+        if (l <= lx && rx <= r) return node->val;
+        int m = (lx + rx) >> 1;
+        return query(node->left, lx, m, l, r) + query(node->right, m + 1, rx, l, r);
+    }
+ 
+    PST(vector<int> &a) {
+        n = a.size();
+        roots.push_back(build(0, n - 1, a));
+    }
+ 
+    void update(int k, int i, int x) {
+        roots[k] = update(roots[k], 0, n - 1, i, x);
+    }
+ 
+    ll query(int k, int l, int r) {
+        return query(roots[k], 0, n - 1, l, r);
+    }
+ 
+    void makeCopy(int k) {
+        roots.push_back(roots[k]);
+    }
+};
+void solve() {
+ int n,q;
+    cin>>n>>q;
+    vector<int>a(n);
+    for (int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    PST pst(a);
+    while (q--) {
+        int op;
+        cin>>op;
+        if (op==1) {
+            int k,val,idx;
+            cin>>k>>idx>>val;
+            k--,idx--;
+ 
+            pst.update(k,idx,val);
+        }
+        else if (op==2) {
+            int k,l,r;
+            cin>>k>>l>>r;
+            k--,l--,r--;
+ 
+            cout<<pst.query(k,l,r)<<el;;
+        }
+        else {
+            int k;cin>>k;
+            k--;
+            pst.roots.push_back(pst.roots[k]);
+        }
+    }
+}
