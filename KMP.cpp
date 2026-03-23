7c390a43ddcb1d7d679a1bfd9059bac5a5ac5e5e
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-03-23 06:12:43 +0200

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -11,3 +11,22 @@ vector<int> kmp(string a) {
     }
     return pi;
 }
+
+
+
+int Matching(vector<int>d1,vector<int>d2) {
+    int n=d1.size(),m=d2.size();
+    int j=0,ans=0;auto p=kmp(d2);
+    for (int i=0;i<n;i++) {
+        while (j>0&&d1[i]!=d2[j]) {
+            j=p[j-1];
+        }
+        if (d1[i]==d2[j])
+            j++;
+        if (j==m) {
+            ans++;
+            j=p[j-1];
+        }
+    }
+    return ans;
+}
