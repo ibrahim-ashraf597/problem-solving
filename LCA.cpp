c473710c2500ad70c6d39316201c3b2762b14aea
ibrahim-ashraf597 <ia9319138@gmail.com>
2026-03-23 11:58:30 +0200

    Refactor LCA implementation into a class structure

--- LCA.cpp
+++ LCA.cpp
@@ -47,3 +47,73 @@ int dis(int u, int v) {
     int lca = LCA(u, v);
     return lvl[u] + lvl[v] - 2 * lvl[lca];
 }
+
+
+
+
+
+
+struct LCA {
+    int n, Log;
+    vector<vector<int>> anc, ad;
+    vector<int> lvl;
+
+    LCA(int n) {
+        this->n = n;
+        Log = 23;
+        anc.assign(n + 1, vector<int>(Log));
+        ad.assign(n + 1, vector<int>());
+        lvl.assign(n + 1, 0);
+    }
+
+    void add_edge(int u, int v) {
+        ad[u].push_back(v);
+        ad[v].push_back(u);
+    }
+
+    void build(int node = 1, int par = 0) {
+        lvl[node] = lvl[par] + 1;
+        anc[node][0] = par;
+
+        for (int i = 1; i < Log; i++) {
+            int p = anc[node][i - 1];
+            anc[node][i] = anc[p][i - 1];
+        }
+
+        for (auto x : ad[node]) {
+            if (x != par) {
+                build(x, node);
+            }
+        }
+    }
+
+    int kth(int node, int k) {
+        for (int i = Log - 1; i >= 0; i--) {
+            if ((k >> i) & 1)
+                node = anc[node][i];
+        }
+        return node;
+    }
+
+    int lca(int u, int v) {
+        if (lvl[u] < lvl[v]) swap(u, v);
+
+        u = kth(u, lvl[u] - lvl[v]);
+
+        if (u == v) return u;
+
+        for (int i = Log - 1; i >= 0; i--) {
+            if (anc[u][i] != anc[v][i]) {
+                u = anc[u][i];
+                v = anc[v][i];
+            }
+        }
+
+        return anc[u][0];
+    }
+
+    int dist(int u, int v) {
+        int L = lca(u, v);
+        return lvl[u] + lvl[v] - 2 * lvl[L];
+    }
+};
